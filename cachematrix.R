177fca7f525e92027169604b232c2563f4cfece8
marthagal <madickinson@gmail.com>
2014-10-22 20:52:57 -0500

    Update cachematrix.R
    completed assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program calculates and caches the inverse value of a matrix
 
-## Write a short comment describing this function
+## This function creates a special matrix. It sets the value of the matrix, gets the value of the matrix, sets the inverse of 
+## the matrix, and gets the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL      #assign local variable m
+  set <- function(y) {
+    x <<- y     
+    m <<- NULL   #assign containing environment m
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve #assign containing environment m
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache. Otherwise,
+## it calculates the inverse and sets the value in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getinverse() #assign local variable m to whatever value is in the env
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...) #assign local variable m 
+  x$setinverse(m)       #this function will assign the set inverse value to m in the containing env
+  m
 }
